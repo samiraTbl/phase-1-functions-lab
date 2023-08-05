28cded1f4e3c51d2bdf0eb90017027d5cc380c68
samira toubal seghir <samsidt@gmail.com>
2023-08-05 18:52:56 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(piLo)
+{
+const scHC =42;
+return Math.abs(scHC-piLo);
+}
+function distanceFromHqInFeet(piLo)
+{
+const disBl=distanceFromHqInBlocks(piLo);
+return disBl*264;
+}
+function distanceTravelledInFeet (sb,db)
+{
+    return Math.abs((db-sb)*264);
+}
+function calculatesFarePrice(sb,db)
+{
+    const dis=distanceTravelledInFeet(sb,db)
+    if (dis <=400 )
+    return 0;
+    if (dis >400 && dis <=2000 )
+        return (dis-400)*0.02
+    else if (dis >2000 && dis <=2500 )
+        return 25;
+    else if (dis>2500)
+        return 'cannot travel that far';
+}
